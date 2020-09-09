b39d8be9e66c7e9d2efe93f43a05cd78ea5e9dcc
martintomas <martintomas.it@gmail.com>
2020-09-09 17:26:58 +0200

    Docker and deployment stuff

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+FROM ruby:2.6.3
+LABEL maintainer="Martin Tomas <martintomas.it@gmail.com>"
+
+RUN mkdir /usr/src/app
+WORKDIR /usr/src/app
+
+# add node
+ENV NODE_VERSION=12.6.0
+RUN apt update
+RUN apt install -y curl postgresql-client
+RUN curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+ENV NVM_DIR=/root/.nvm
+RUN . "$NVM_DIR/nvm.sh" && nvm install ${NODE_VERSION}
+RUN . "$NVM_DIR/nvm.sh" && nvm use v${NODE_VERSION}
+RUN . "$NVM_DIR/nvm.sh" && nvm alias default v${NODE_VERSION}
+ENV PATH="/root/.nvm/versions/node/v${NODE_VERSION}/bin/:${PATH}"
+RUN node --version
+RUN npm --version
+RUN npm install --global yarn
+
+ARG INSTALL_CHROMIUM
+RUN sh -c 'if [ "$INSTALL_CHROMIUM" = true ]; then apt install -y chromium; else echo "Skipping chrome installation"; fi'
+
+COPY Gemfile Gemfile.lock ./
+RUN bundle install
+
+COPY package.json ./
+RUN yarn install --check-files
+
+COPY . .
+
+ENTRYPOINT ["./entrypoint.sh"]
+CMD ["start-server"]
---
+++ entrypoint.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -e
+
+case ${RAILS_ENV} in
+    *)
+        echo "Environment: ${RAILS_ENV}"
+esac
+
+start_server() {
+    if [[ "${RAILS_ENV}" = "development" ]]; then
+        echo "Starting server in ${RAILS_ENV} mode. Skipping db:migrate and assets:precompile"
+        rm -rf tmp/pids/server.pid
+        bundle exec rails server --port 3000 --binding 0.0.0.0 -e ${RAILS_ENV}
+    else
+        echo "Starting server ${RAILS_ENV} mode. Running db:migrate and assets:precompile"
+        rails webpacker:install && rails db:migrate && rake assets:precompile && bundle exec rails server --port 3000 --binding 0.0.0.0 -e ${RAILS_ENV}
+    fi
+}
+
+start_background_job() {
+    echo "Starting background job in ${RAILS_ENV} mode"
+    bundle exec sidekiq -C config/sidekiq.yml
+}
+
+if [[ "$1" = "start-server" ]]; then
+    start_server
+
+elif [[ "$1" = "start-background-job" ]]; then
+    start_background_job
+
+else
+    echo "No known command given at entrypoint. Running exec \"$@\""
+    exec "$@"
+fi
