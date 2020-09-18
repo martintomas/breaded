f54681d6226143389439a6595fc14aebe13a9ddb
martintomas <martintomas.it@gmail.com>
2020-09-18 12:14:47 +0200

    fix of env variables

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,6 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV["REDIS_URL"], password: ENV["REDIS_PASSWORD"] }
+  config.redis = { url: ENV["REDIS_URL"], password: ENV["REDIS_PASS"] }
 end
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV["REDIS_URL"], password: ENV["REDIS_PASSWORD"] }
+  config.redis = { url: ENV["REDIS_URL"], password: ENV["REDIS_PASS"] }
 end
