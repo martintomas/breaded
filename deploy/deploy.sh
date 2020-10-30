a574fcd2dbb6d60bbdd0de8229471b64e11290d3
martintomas <martintomas.it@gmail.com>
2020-10-30 08:30:44 +0100

    update of deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -19,6 +19,8 @@ docker tag breaded-production:$COMMIT_HASH registry.digitalocean.com/breaded/bre
 docker push registry.digitalocean.com/breaded/breaded-production:$COMMIT_HASH
 
 kubectl config use-context do-lon1-breaded-cluster
+kubectl apply -f web-env-configmap.yaml
+kubectl apply -f breaded-secrets.yaml
 kubectl set image deployment/web web=registry.digitalocean.com/breaded/breaded-production:$COMMIT_HASH
 kubectl set image deployment/background-job background-job=registry.digitalocean.com/breaded/breaded-production:$COMMIT_HASH
 
